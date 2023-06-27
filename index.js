2aeb03559a8299c9ee819bc597136ce4265716d9
arekkukula <arkadiusz.kukula@gmail.com>
2023-06-27 02:29:53 +0200

    Receive buf requests on server

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
+import path from "path";
+
 import http from "http";
 import express from "express";
 const app = express()
 
+const __dirname = path.resolve();
+
 http.createServer(app).listen(3000);
 
+app.use("/shared", express.static(__dirname + "/shared"));
+
 app.get("/", (req, res) => {
-    res.sendFile("views/index.html");
+    res.sendFile(__dirname + "/views/index.html");
 });
 
-app.get("/api/buf", (req, res) => {
-    
+app.post("/api/buf", (req, res) => {
+    req.setEncoding("binary");
+
+    const chunks = [];
+
+    req.on("data", chunk => {
+        chunks.push(Buffer.from(chunk, "binary"));
+    });
+
+    req.on("end", () => {
+        const buf = Buffer.concat(chunks);
+        const view = new Uint8Array(buf);
+        console.log(view);
+        const user = User.prototype.from(buf);
+        console.log(user);
+        res.sendStatus(200);
+    });
 });
 
 import { User } from "./shared/user.js";
-new User(1, "First Name", "Second Name").encode();
+new User(123456789, "First Name", "Second Name").encode();
 
