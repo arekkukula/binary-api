01a253dd0e7c01c34d6f6fa5d02c360591c714f6
arekkukula <arkadiusz.kukula@gmail.com>
2023-06-28 02:56:01 +0200

    Expose buf and json endpoints

--- index.js
+++ index.js
@@ -5,34 +5,53 @@ import express from "express";
 const app = express()
 
 const __dirname = path.resolve();
+import { User } from "./shared/user.js";
 
 http.createServer(app).listen(3000);
 
 app.use("/shared", express.static(__dirname + "/shared"));
+app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 });
 
+let chunks = [];
+let user = new User();
 app.post("/api/buf", (req, res) => {
     req.setEncoding("binary");
-
-    const chunks = [];
-
-    req.on("data", chunk => {
-        chunks.push(Buffer.from(chunk, "binary"));
+    req.on("data", ch => {
+        chunks.push(ch);
     });
 
     req.on("end", () => {
-        const buf = Buffer.concat(chunks);
-        const view = new Uint8Array(buf);
-        console.log(view);
-        const user = User.prototype.from(buf);
-        console.log(user);
-        res.sendStatus(200);
+        user.fromBuffer(nodeBufferToArrayBuffer(chunks[0]));
+        chunks.splice(0, chunks.length);
+        res.send(req.headers["content-length"]);
     });
 });
 
-import { User } from "./shared/user.js";
-new User(123456789, "First Name", "Second Name").encode();
+app.post("/api/json", async (req, res) => {
+    const json = req.body;
+    const user = new User(json.id, json.firstName, json.secondName);
+
+    res.send(req.headers["content-length"]);
+});
+
+/** 
+    * @param {Buffer} buf
+    * @returns {ArrayBuffer}
+*/
+function nodeBufferToArrayBuffer(buf) {
+    const ab = new ArrayBuffer(buf.byteLength);
+
+    const abView = new Uint8Array(ab);
+    const bufView = new Uint8Array(buf);
+    for (let i = 0; i < buf.byteLength; i++) {
+        abView[i] = bufView[i];
+    }
+
+    return ab;
+}
+
 
